850f436279aebbd4fc50023307ad983e22cb9e6e
Vijith VS <vijithvenganoor@gmail.com>
2016-01-14 17:51:36 +0530

    Project Check in

---
+++ TimeSheetReminder/TimeSheetReminder/Controllers/TimeSheetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TimeSheetReminder.Controllers
+{
+    public class TimeSheetController : ApiController
+    {
+        [HttpGet]
+        public bool CheckTime()
+        {
+
+            return true;
+        }
+    }
+}
---
+++ TimeSheetReminder/TimeSheetReminder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimeSheetReminder.WebApiApplication" Language="C#" %>
